e1a67347eec2cf36518c42c22d3f07e8fee56ffc
Bernardus Molkenboer <bart@backdigital.nl>
2024-11-10 13:43:47 +0700

    Fix Header logo

--- src/layout/header/Logo.tsx
+++ src/layout/header/Logo.tsx
@@ -1,10 +1,20 @@
 import LogoSchilder from "@/svg/logo";
+import LogoSchilderWit from "@/svg/logoWit";
 import Link from "next/link";
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center">
-      <LogoSchilder className="h-10 max-h-[35px] w-auto" aria-label="Logo" />
+      {/* Logo for light theme */}
+      <LogoSchilder
+        className="block h-10 max-h-[35px] w-auto dark:hidden"
+        aria-label="Logo"
+      />
+      {/* Logo for dark theme */}
+      <LogoSchilderWit
+        className="hidden h-10 max-h-[35px] w-auto dark:block"
+        aria-label="Logo"
+      />
     </Link>
   );
 }
