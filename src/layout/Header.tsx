87ee85e563eaacf68c7f70b3a85577dad06f28de
Bernardus Molkenboer <bart@backdigital.nl>
2024-11-10 12:42:54 +0700

    Fix Styling Typografy broken for some reason

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,6 +4,7 @@ import { MobileMenu } from "./header/MobileMenu";
 
 function Header() {
   return (
+    //
     <header className="fixed z-50 w-full border-b border-border bg-white text-black dark:bg-[#141414] dark:text-white">
       <div className="mx-auto max-w-screen-xl px-6">
         <div className="flex h-16 items-center justify-between">
